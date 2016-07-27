549966d9503b99a0535ae6a4e157982b3ef70761
ardeshir sepahsalar <ardeshir.org@gmail.com>
2016-07-27 17:32:31 +0000

    added code

--- src/App.js
+++ src/App.js
@@ -8,11 +8,18 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1>Welcome to univrs.io</h1>
+          <h1>Welcome to learning to learn on Metalearn.org</h1>
         </div>
         <p className="App-intro">
-          To get started click <code><a href="http://univrs.io">univrs.io</a></code>
+          Use this platform to learn ReactJS!
         </p>
+        <div className="divleft">
+          <ul>
+            <li>Login to cloud9 @ <a href="http://metalearn.org:8080">metalearn.org cloud9 IDE (live coding)</a></li>
+            <li>Navigate to ~/dem0/src/App.js</li>
+            <li>The git repository is @<a href="https://github.com/ardeshir/dem0">github.com/ardeshir/dem0</a></li>
+          </ul>
+        </div>
       </div>
     );
   }
