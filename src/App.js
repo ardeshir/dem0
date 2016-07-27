e5e69c802fbc33965dd4f7ef7b8f6d1f5dfb85dd
ardeshir sepahsalar <ardeshir.org@gmail.com>
2016-07-27 17:34:16 +0000

    fixed bug

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1>Welcome to learning to learn on Metalearn.org</h1>
+          <h1>Welcome to learning to learn: Metalearn.org</h1>
         </div>
         <p className="App-intro">
           Use this platform to learn ReactJS!
